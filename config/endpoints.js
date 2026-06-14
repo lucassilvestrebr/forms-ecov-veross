877baeeab548b1bb6e3257edf1163480ed867a0f
lucassilvestrebr <lucas.silvestre@veross.com.br>
2026-06-14 14:26:09 -0300

    Delete config directory

--- config/endpoints.js
+++
@@ -1,6 +0,0 @@
-// Veross — Endpoints de Integração
-// Edite apenas este arquivo para atualizar as URLs dos Apps Script.
-
-var ENDPOINTS = {
-  diagnosticoEcov: 'https://script.google.com/a/macros/veross.com.br/s/AKfycby6NA5r6ZAPbdwRdwoRHvRtBTrxfMLN4sN6RVVIqUmjhkvyJToW_ewtamWobxC9dgAj/exec'
-};
