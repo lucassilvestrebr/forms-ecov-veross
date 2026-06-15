2b9686e0e3a8bed8ea06564c0c56c299f6b5866d
lucassilvestrebr <lucas.silvestre@veross.com.br>
2026-06-15 12:03:58 -0300

    Add files via upload

--- config/endpoints.js
+++ config/endpoints.js
@@ -2,5 +2,5 @@
 // Edite apenas este arquivo para atualizar as URLs dos Apps Script.
 
 var ENDPOINTS = {
-  diagnosticoEcov: 'https://script.google.com/a/macros/veross.com.br/s/AKfycby6NA5r6ZAPbdwRdwoRHvRtBTrxfMLN4sN6RVVIqUmjhkvyJToW_ewtamWobxC9dgAj/exec'
+  diagnosticoEcov: 'https://script.google.com/a/macros/veross.com.br/s/AKfycbwh1xxitHaGCCmjs1yUXnUuYX9lz4EY6uV1zleN7UNmwT4sBQw2oOHOIlU5adbtl55jTQ/exec'
 };
